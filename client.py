b141cc4d1c3806c533d36c526b724cb4cfcfb2b7
kate <katarzynadudziak94@gmail.com>
2022-10-18 15:18:04 +0200

    First working version

--- client.py
+++ client.py
@@ -1,41 +1,46 @@
 import socket
-import threading
+from threading import Thread
+
 
 nickname = input("Write your nickname: ") 
 
 HOST = '127.0.0.1'
 PORT = 3891
 
-
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
 
-def receive_message(client):
-    while True:
-        try:
+def receive_message():
+    try:
+        message = True
+        while message:  # if message == -1 then connection is closed
             message = client.recv(1024).decode()
             if message == 'nick':
                 client.send(nickname.encode())
             else:
                 print(message)
-        except:
-            print('error')
-            client.close()
+    except Exception as ex:
+        print(ex)
+        client.close()
 
 
 def write_message(client):
     while True:
-        message = f'{nickname}: {input()}'
-        client.send(message.encode())
+        try:
+            message = f'{nickname}: {input()}'
+            client.send(message.encode())
+        except:
+            print('cannot send message')
+            client.close()
 
 
 def main():
-    receive_thread = threading.Thread(target=receive_message, args=(client,))
-    receive_thread.start()
 
-    write_thread = threading.Thread(target=write_message, args=(client,))
-    write_thread.start()
+    receive_thread = Thread(target=receive_message, )
+    receive_thread.start()
+    write_message(client)   
+
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 import socket
-import threading
-
-clients = {}
+from threading import Thread
 
 HOST = '127.0.0.1'
 PORT = 3891
@@ -14,42 +12,52 @@ def create_socket():
     return s
 
 
-def add_client(socket):
-    client, address = socket.accept()
-    client.send('nick'.encode())
-    nickname = client.recv(1024).decode()
-    clients[nickname] = client
-
-    print(f'Client {nickname}, has joined us with address: {address}')
-    message = f"Hello {nickname} from server to client"
-    client.send(message.encode())
-
-    thread = threading.Thread(target=handle, args=(client,))
-    thread.start()
-
-    return client, nickname
-
-
-def handle(client, nickname):
+def handle_client(socket, clients):
     while True:
-        try:
-            message = client.recv(1024).decode()
-            broadcast(message)
+        conn, addr = socket.accept()
+        conn.send('nick'.encode())
+        nickname = conn.recv(1024).decode()
+        clients[addr] = (conn, nickname)
+
+        print(f'Client {nickname} joined')                      # te 4 linie aż proszą się o zamknięcie w funkcję i nazwanie tego kawałka, nawet
+        message = f'Hello {nickname} from server to client'     # oddzieliłaś je pustymi liniami, czyli tworzą jakąś logiczną całość
+        conn.send(message.encode())
+        broadcast(f'{nickname} joined to server', clients, conn)
+
+        thread_handle_message = Thread(target=handle_message, args=(conn, addr,nickname, clients,  ))
+        thread_handle_message.start()
+
+
+def handle_message(conn, addr, nickname, clients):  # gdy znamy addr (klucz) to conn i nickname można wyciągnąć ze słownika clients,
+    while True:                                     # nie trzeba ich przekazywać w parametrach.
+        try:                                        # W tym celu możesz napisać funkcje pomocnicze np. get_nickname() i get_connection() podając
+            received_data = conn.recv(1024)         # w parametrach słownik oraz klucz (czyli addr)
+            if not received_data:
+                broadcast(f'{nickname} left from server', clients, conn)
+                del clients[addr]
+                break
+
+            message = received_data.decode()
+            broadcast(message, clients, conn)
         except:
-            clients.pop(nickname)
-            client.close()
+            conn.close()                            # w tym przypadku nie usuniemy klienta ze słownika ani nie poinformujemy innych, a chcielibyśmy
+            break
 
 
-def broadcast(message):
-    for client in clients:
-        client.send(message.encode())
+def broadcast(message, clients, connection):
+    for conn, _ in clients.values():
+        if conn != connection:
+            conn.send(message.encode())
 
 
 def main():
-    socket = create_socket()
-    client, nickname = add_client(socket)
-    handle(client, nickname)
+    clients = dict()
 
+    socket = create_socket()
+    thread_handle_client = Thread(target=handle_client, args=(socket, clients))   # to samo co na początku, przecinek jest zbędny
+
+    thread_handle_client.start()
+    thread_handle_client.join()
 
 if __name__ == "__main__":
     main()
