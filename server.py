00a677c37501c88882e39580d280e34fdc7eb471
Kasia <katarzynadudziak94@gmail.com>
2023-02-06 16:30:01 +0100

    Added ClientHandler, MessageHandler and ServerClient classes  to server

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
-import socket, time, sys
+import socket
+import time
+import sys
 import threading
 from threading import Thread, Event
 from message import Message
-from datetime import datetime
+import queue
 
 
 class Server:
@@ -10,30 +12,23 @@ class Server:
         self.host = host
         self.port = port
         self.clients = dict()
+        self.q = queue.Queue()
 
-    def create_socket(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((self.host, self.port))
-        s.listen()
-        return s
-
-    def handle_clients(self, socket):
-        while True:
-            self.handle_one_client(socket)
-
-    def handle_one_client(self, socket):
-        addr = self.accept_client(socket)
-
-        thread_handle_message = Thread(target=self.handle_messages, args=(addr,))
-        thread_handle_message.start()
-
-    def accept_client(self, socket):
-        conn, addr = socket.accept()
-        conn.send('nick'.encode())
-        nickname = conn.recv(1024).decode()
-        self.clients[addr] = (conn, nickname)
-        self.send_server_messages(addr)
-        return addr
+    def start_server(self):
+        try:
+            client_handler = ClientHandler(self.host, self.port, self.q)
+            client_handler.start()
+            while True:
+                event = self.q.get()
+                if isinstance(event, ServerClient):
+                    # MessageHandler(event)
+                    print(f'client {event.nickname} connected')
+                else:
+                    pass #todo
+        #     while client_handler.is_alive():
+        #         client_handler.join(1)
+        except KeyboardInterrupt:
+            sys.exit()
 
     def send_server_messages(self, addr):
         nickname = self.get_nickname(addr)
@@ -105,19 +100,54 @@ class Server:
                 conn.send(message)
 
 
+class ClientHandler(Thread):
+    def __init__(self, host, port, q):
+        super().__init__()
+        self.host = host
+        self.port = port
+        self.q = q
+        self.create_socket()
+
+    def create_socket(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((self.host, self.port))
+        self.socket = s
+
+    def accept_client(self):
+        conn, addr = self.socket.accept()
+        conn.send('nick'.encode())
+        nickname = conn.recv(1024).decode()
+        server_client = ServerClient(conn, addr, nickname)
+        self.q.put(server_client)
+        
+    def run(self):
+        self.socket.listen()
+        while True:
+            self.accept_client()
+
+
+class MessageHandler(Thread):
+    def __init__(self, server_client):
+        super().__init__()
+        self.server_client = server_client
+
+    def run(self):
+        pass
+
+
+class ServerClient:
+    def __init__(self, conn, addr, nickname):
+        self.conn = conn
+        self.addr = addr
+        self.nickname = nickname
+
+
 def main():
     host = '127.0.0.1'
     port = 3889
 
     server = Server(host, port)
-    socket = server.create_socket()
-    try:
-        thread_handle_clients = Thread(target=server.handle_clients, args=(socket,))
-        thread_handle_clients.start()
-        while thread_handle_clients.is_alive():
-            thread_handle_clients.join(1)
-    except KeyboardInterrupt:
-        sys.exit()
+    server.start_server()
 
 
 if __name__ == "__main__":
