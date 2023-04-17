9807aa9d9e0eff5f53ad9640b928bbb26cc3d4f3
Kasia <katarzynadudziak94@gmail.com>
2023-04-17 12:58:25 +0200

    Tests refactor

--- tests/test_client.py
+++ tests/test_client.py
@@ -10,6 +10,7 @@ def test_create_client(mock_socket):
     port = 123
     receive_callback = MagicMock()
     client = Client(host, port, 'nickname', receive_callback)
+
     mock_socket.return_value.connect.assert_called_once_with((host, port))
 
 
@@ -23,7 +24,9 @@ def test_close_connection_on_exception(mock_Message, mock_socket, mock_Thread):
     client = Client(host, port, 'nickname', receive_callback)
     client.connection.recv.return_value.decode.return_value = False
     obj_message = mock_Message
+
     client.receive_message()
+
     receive_callback.assert_called_once_with(obj_message.return_value)
 
 
@@ -35,6 +38,7 @@ def test_catch_exception_in_handle_recv_message(mock_socket, mock_Thread):
     receive_callback = MagicMock()
     client = Client(host, port, 'nickname', receive_callback)
     client.connection.recv.return_value.decode.return_value = False
+
     with pytest.raises(Exception) as ex:
         client.handle_recv_message()
         assert ex.type is Exception
@@ -48,7 +52,9 @@ def test_handle_nick_message(mock_socket, mock_Thread):
     receive_callback = MagicMock()
     client = Client(host, port, 'nickname', receive_callback)
     client.connection.recv.return_value.decode.return_value = 'nick'
+
     client.handle_recv_message()
+
     client.connection.send.assert_called_once_with(client.nickname.encode())
 
 
@@ -61,7 +67,9 @@ def test_handle_correct_message(mock_Message, mock_socket, mock_Thread):
     receive_callback = MagicMock()
     client = Client(host, port, 'nickname', receive_callback)
     client.connection.recv.return_value.decode.return_value = 'something'
+
     client.handle_recv_message()
+
     mock_Message.return_value.convert_to_obj.assert_called_once_with('something')
     client.receive_callback.assert_called_once_with(mock_Message.return_value)
 
@@ -75,7 +83,9 @@ def test_catch_exception_in_write_message(mock_message, mock_socket, mock_Thread
     receive_callback = MagicMock()
     client = Client(host, port, 'nickname', receive_callback)
     mock_message.return_value.encode.side_effect = Exception
+
     client.write_message(mock_message.return_value)
+
     client.connection.close.assert_called_once()
 
 
@@ -88,7 +98,9 @@ def test_send_correct_message_to_server(mock_message, mock_socket, mock_Thread):
     receive_callback = MagicMock()
     client = Client(host, port, 'nickname', receive_callback)
     mock_message.encode.return_value = 'something'
+
     client.write_message(mock_message)
+
     client.connection.send.assert_called_once_with('something')
 
 
@@ -99,5 +111,7 @@ def test_stop_connection(mock_socket, mock_Thread):
     port = 123
     receive_callback = MagicMock()
     client = Client(host, port, 'nickname', receive_callback)
+
     client.stop()
+    
     client.connection.close.assert_called_once()
--- tests/test_server.py
+++ tests/test_server.py
@@ -17,7 +17,9 @@ def test_client_joined_to_server(mock_Queue, mock_ClientHandler,mock_handle_clie
     server_client_mock = MagicMock()
     event = (server_client_mock, Event_Type.ServerClient)
     mock_Queue.return_value.get.side_effect = [event, Empty, KeyboardInterrupt]
+
     server.start_server()
+
     mock_ClientHandler.return_value.start.assert_called_once()
     server_client_mock.start.assert_called_once()
     mock_Event.return_value.set.assert_called_once()
@@ -35,7 +37,9 @@ def test_get_message_from_client(mock_handle_message, mock_Queue, mock_ClientHan
     message_mock = MagicMock()
     event = (message_mock, Event_Type.MessageToServer)
     mock_Queue.return_value.get.side_effect = [event, KeyboardInterrupt]
+
     server.start_server()
+
     mock_handle_message.assert_called_once_with(message_mock)
     
 
@@ -50,7 +54,9 @@ def test_handle_lef_message(mock_remove_user, mock_broadcast, mock_buid_user_lef
     mock_MessageToServer.message = 'left'
     left_message = 'message'
     mock_buid_user_left_message.return_value = left_message
+
     server.handle_message(mock_MessageToServer)
+
     mock_broadcast.assert_called_once_with(left_message, mock_MessageToServer.sender)
     mock_remove_user.assert_called_once_with(mock_MessageToServer)
 
@@ -62,7 +68,9 @@ def test_normal_message(mock_broadcast, mock_MessageToServer):
     port = 123
     server = Server(host, port)
     mock_MessageToServer.message = 'message'
+
     server.handle_message(mock_MessageToServer)
+
     mock_broadcast.assert_called_once_with(mock_MessageToServer.message, mock_MessageToServer.sender)
 
 
@@ -74,8 +82,9 @@ def test_remove_user(mock_send_server_messages):
     client = MagicMock()
     client.conn = 'conn'
     client.sender = 'conn'
-    server.handle_client(client)
+
     server.remove_user(client)
+    
     assert server.clients == []
 
 
@@ -85,7 +94,9 @@ def test_send_welcome_message(mock_Message, mock_ServerClient):
     host = '123'
     port = 123
     server = Server(host, port)
+
     server.send_welcome_message(mock_ServerClient)
+
     mock_ServerClient.conn.send.assert_called_once_with(mock_Message.return_value.encode.return_value)
 
 
@@ -96,7 +107,9 @@ def test_send_client_join_message(mock_Message, mock_ServerClient, mock_broadcas
     host = '123'
     port = 123
     server = Server(host, port)
+
     server.send_message_about_client_join(mock_ServerClient)
+
     mock_broadcast.assert_called_once_with(mock_Message.return_value.encode.return_value, mock_ServerClient.conn)
 
 
@@ -106,7 +119,9 @@ def test_build_client_left_message(mock_Message, mock_ServerClient):
     host = '123'
     port = 123
     server = Server(host, port)
+
     message = server.build_client_left_message(mock_ServerClient)
+
     assert message
 
 
@@ -118,7 +133,9 @@ def test_broadcast():
     message = MagicMock()
     element.conn = 'conn'
     conn = 'conn'
+
     server.broadcast(message, conn)
+
     element.return_value.conn.send(message)
 
 
@@ -143,7 +160,9 @@ def test_accept_client(mock_socket, mock_q):
     server_client = ServerClient(conn, addr, nickname, mock_q, 'event')
     mock_socket.return_value.accept.return_value = conn, addr
     conn.recv.return_value.decode.return_value = nickname
+
     client_handler.accept_client()
+
     conn.settimeout.assert_called_once_with(1)
     conn.send.assert_called_once_with('nick'.encode())
     mock_q.put.return_value((server_client, Event_Type.ServerClient))
@@ -159,7 +178,9 @@ def test_raise_timeout_exception_in_accept_client(mock_socket):
     addr = MagicMock()
     mock_socket.return_value.accept.return_value = conn, addr
     conn.send.side_effect = timeout
+
     client_handler.accept_client()
+
     conn.settimeout.assert_called_once_with(1)
     q.put.assert_not_called()
     
@@ -172,7 +193,9 @@ def test_run_client_handler(mock_socket, mock_accept_client):
     event = MagicMock()
     client_handler = ClientHandler(host, port, 'q', event)
     event.is_set.side_effect = [False, True]
+
     client_handler.run()
+
     mock_socket.return_value.listen.assert_called_once()
     mock_accept_client.assert_called_once()
 
@@ -187,7 +210,9 @@ def test_handle_recv_valid_data(mock_queue_message):
     event.is_set.side_effect = [False, True]
     received_data = 'message'
     conn.recv.return_value = received_data
+
     message_handler.handle_received_data()
+
     mock_queue_message.assert_called_once_with(received_data)
 
 
@@ -199,6 +224,7 @@ def test_no_recv_data():
     message_handler = MessageHandler(conn, nickname, q, event)
     event.is_set.side_effect = [False]
     conn.recv.return_value = False
+
     with pytest.raises(Exception) as ex:
         message_handler.handle_received_data()
         assert ex.type is Exception
@@ -212,7 +238,9 @@ def test_raise_timeout_exception_in_handle_recv_data():
     message_handler = MessageHandler(conn, nickname, q, event)
     event.is_set.side_effect = [False, False, True]
     conn.recv.return_value = timeout
+
     message_handler.handle_received_data()
+
     assert 2 == conn.recv.call_count
 
 
@@ -226,7 +254,9 @@ def test_left_message_recv(mock_queue_message):
     event.is_set.side_effect = [False, True]
     received_data = 'left'
     conn.recv.return_value = received_data
+
     message_handler.handle_received_data()
+
     mock_queue_message.assert_called_once_with(received_data)
 
 
@@ -238,7 +268,9 @@ def test_put_objects_to_queue(mock_MessageToServer):
     conn = 'conn'
     message_handler = MessageHandler(conn, nickname, q, event)
     message = 'message'
+
     message_handler.queue_message(message)
+
     q.put.assert_called_once_with((mock_MessageToServer.return_value, Event_Type.MessageToServer))
 
 
@@ -249,8 +281,8 @@ def test_run_message_handler(mock_handle_recv_data):
     event = 'event'
     conn = MagicMock()
     message_handler = MessageHandler(conn, nickname, q, event)
+
     message_handler.run()
+
     mock_handle_recv_data.assert_called_once()
     conn.close.assert_called_once()
-    
-    
